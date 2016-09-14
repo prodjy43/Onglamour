eb9c9f85eb2ddde8e348b9563e16f35844f62cbb
prodjy43 <prodjy43@gmail.com>
2016-09-14 10:21:37 +0200

    mail verif

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,18 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests;
 use App\Meeting;
 
 class AdminController extends Controller
 {
+	private $id;
+
     public function showRdv(){
     	$meetings = Meeting::where('validate', 0)->join('users', 'user_id', '=', 'users.id')->get();
     	return view('admin.meetings', ['title' => 'Rendez-vous', 'meetings' => $meetings]);
     }
 
-    public function accpetRdv($id){
-    	
+    public function acceptRdv($id){
+    	$this->id = $id;
+    	$meeting = Meeting::where('id_meeting', $id)->join('users', 'user_id', '=', 'users.id')->first();
+    	$this->updateRdv($id);
+    	Mail::send('emails.accept', ['meeting' => $meeting], function($message){
+    		$meeting = Meeting::where('id_meeting', $this->id)->join('users', 'user_id', '=', 'users.id')->first();
+    		$message->from('sdbiedermann@bluewin.ch', 'Daniela Biedermann');
+
+    		$message->to($meeting->email);
+    	});
+
+    	return response()->json(['message' => 'Request completed']);
+    }
+
+    protected function updateRdv($id){
+    	return Meeting::where('id_meeting', $id)->update([
+    			'validate' => 1,
+    		]);
     }
 }
---
+++ resources/views/emails/accept.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+<body>
+<h1>Rendez-vous accepté</h1>
+<p>Votre rendez-vous du {{ $meeting->date }} à {{ $meeting->heure }} a été accepté</p>
+<p>Si vous voulez quelqu'onque autre informations vous pouvez m'appeler au (+41)79/769.27.64 ou m'envoyer un mail à sdbiedermann@gmail.com</p>
+</body>
+</html>
